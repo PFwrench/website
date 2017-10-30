93078d8af40954917393351dd3e9e8a758e0f3bc
Peter French <peterfrench@Peters-MacBook-Pro.local>
2017-10-29 23:48:26 -0500

    Added favicon and a few other things

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Peter French' });
 });
 
 module.exports = router;
