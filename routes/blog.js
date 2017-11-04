9eb4524ff0f6c5c8556ae3b51aa201ed8486d0f1
Peter French <peterfrench@Peters-MacBook-Pro.local>
2017-11-04 03:12:17 -0500

    Missed comments

--- routes/blog.js
+++ routes/blog.js
@@ -17,44 +17,6 @@ var posts = [];
 
 mdhtml.populate(src, dest, linkDest);
 
-// fs.readdir(src, function(err, list) {
-//   if(err) {
-//     console.log('ERROR READING POST MD FILES');
-//   } else {
-//     for (var i = 0; i < list.length; i++) {
-//       fs.readFile(src + list[i], function(err, data) {
-//         if(err) {
-//           console.log('ERROR READING MD FILE');
-//         } else {
-//           var meta = yaml.loadFront(data);
-//           var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-//           var date = new Date(meta.date);
-//           var mon = date.getMonth();
-//           var day = date.getDate();
-//           var year = date.getFullYear();
-//           var date = months[mon] + " " + day + " " + year;
-//           var renderedPost = md.render(meta.__content);
-//           var dateobj = new Date(date);
-//           var pth = dateobj.getFullYear() + "/" + dateobj.getMonth() + "/" + dateobj.getDate() + "/";
-//
-//           meta.path = linkDest + pth;
-//           meta.dateString = date;
-//           posts.push(meta);
-//
-//           mkdirp("./" + dest + pth, function (err) {
-//             if (err) console.log(err);
-//             fs.writeFile(__dirname + "/.." + dest + pth + "index.html", renderedPost, function(err) {
-//               if (err) {
-//                 console.log("ERROR in " + __dirname + " : "+ err);
-//               }
-//             });
-//           });
-//         }
-//       });
-//     }
-//   }
-// });
-
 /* GET blog page */
 router.get('/', function(req, res, next) {
   res.render('blog', {
