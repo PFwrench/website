cb64c4bc49ec27f6b71676ab90d5c235e355dda2
Peter French <peterfrench@Peters-MacBook-Pro.local>
2017-11-06 01:51:52 -0600

    Fixed browser issues and some more style things

--- public/javascripts/toggleBlog.js
+++ public/javascripts/toggleBlog.js
@@ -27,12 +27,17 @@ $(function () {
     });
   });
 
-  $('.articleLink').click(function () {
+  $('.post').click(function (event) {
+    if (!event) event = window.event;
     $(".blog").removeClass("here");
     $(".blog").addClass("gone");
     $(".none").addClass("gone");
     $(".posts>*").addClass("viewPost");
-    var link = $(event.target).attr('data');
+    var link = $(event.target).attr("data");
+    console.log(event.target);
+    if (link == undefined) {
+      link = $(event.target).closest(".post").attr("data");
+    }
     $(".posts>*").one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function() {
       window.location.replace("http://localhost:3000" + link);
     });
